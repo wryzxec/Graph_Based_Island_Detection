447e22f0beb6ef21b5b4f541d780586edfbb45fa
Lucas Bruckbauer <lucas.bruckbauer@gmail.com>
2024-01-03 18:15:54 +0000

    Added slider to change cell size - implemented additional UI logic to disable changes during visualisation which may lead to unexpected errors.

--- src/main/java/com/example/island_algorithm_visualiser/Controller.java
+++ src/main/java/com/example/island_algorithm_visualiser/Controller.java
@@ -1,10 +1,9 @@
     package com.example.island_algorithm_visualiser;
 
+    import javafx.beans.NamedArg;
     import javafx.fxml.FXML;
     import javafx.fxml.Initializable;
-    import javafx.scene.control.Button;
-    import javafx.scene.control.CheckBox;
-    import javafx.scene.control.Label;
+    import javafx.scene.control.*;
     import javafx.scene.input.MouseEvent;
     import javafx.scene.layout.AnchorPane;
     import javafx.scene.layout.BorderPane;
@@ -25,7 +24,7 @@
         @FXML
         private Button start_button;
         @FXML
-        private Button stop_button;
+        private Button reset_button;
         @FXML
         private Button generate_new_button;
         @FXML
@@ -50,16 +49,37 @@
         private CheckBox speed_checkbox_2x;
         @FXML
         private CheckBox show_perimeter_checkbox;
+        @FXML
+        private Slider cell_size_slider;
 
         private Grid grid;
-        private int gridSize = 35;
+        private int gridSize;
         private int[][] values;
         private boolean[][] visited;
         private GridHandler gridHandler;
         private Statistics statistics;
 
+        public void enableSettingsChanges(){
+            DFS_CheckBox.setDisable(false);
+            BFS_CheckBox.setDisable(false);
+            speed_checkbox_1x.setDisable(false);
+            speed_checkbox_2x.setDisable(false);
+            show_perimeter_checkbox.setDisable(false);
+            cell_size_slider.setDisable(false);
+        }
+
+        public void disableSettingsChanges(){
+            DFS_CheckBox.setDisable(true);
+            BFS_CheckBox.setDisable(true);
+            speed_checkbox_1x.setDisable(true);
+            speed_checkbox_2x.setDisable(true);
+            show_perimeter_checkbox.setDisable(true);
+            cell_size_slider.setDisable(true);
+        }
+
         @FXML
         void startButtonClick(MouseEvent event) {
+            disableSettingsChanges();
             gridHandler.setShowPerimeterSelected(show_perimeter_checkbox.isSelected());
             gridHandler.setBFS_Selected(BFS_CheckBox.isSelected());
             gridHandler.setDFS_Selected(DFS_CheckBox.isSelected());
@@ -71,7 +91,8 @@
             }
         }
         @FXML
-        void stopButtonClick(MouseEvent event){
+        void resetButtonClick(MouseEvent event){
+            enableSettingsChanges();
             grid_pane.getChildren().clear();
             gridHandler.stopVisualisation();
             gridHandler.resetVisited();
@@ -81,6 +102,8 @@
 
         @FXML
         void generateNewButtonClick(MouseEvent event) {
+            enableSettingsChanges();
+            gridSize = (int) cell_size_slider.getValue();
             if(gridHandler.isVisualizationRunning()){
                 gridHandler.stopVisualisation();
             }
@@ -90,7 +113,6 @@
             visited = new boolean[(int) grid_pane.getPrefHeight() / gridSize][(int) grid_pane.getPrefWidth() / gridSize];
             gridHandler = new GridHandler(grid_pane.getPrefWidth(), grid_pane.getPrefHeight(), gridSize, grid_pane, values, visited, statistics);
             gridHandler.initializeGrid();
-            System.out.println(grid_pane.getChildren().size());
         }
 
         @FXML
@@ -106,14 +128,29 @@
             speed_checkbox_2x.setSelected(false);
         }
         @FXML
-        public void speed2xSelected(MouseEvent event){
+        public void speed2xSelected(MouseEvent event) {
             speed_checkbox_1x.setSelected(false);
+        }
+
+        @FXML
+        public void cellSizeSliderReleased(MouseEvent event){
+            if(gridSize != (int) cell_size_slider.getValue()){
+                gridSize = (int) cell_size_slider.getValue();
+                statistics.resetStats();
+                grid_pane.getChildren().clear();
+                values = new int[(int) grid_pane.getPrefHeight() / gridSize][(int) grid_pane.getPrefWidth() / gridSize];
+                visited = new boolean[(int) grid_pane.getPrefHeight() / gridSize][(int) grid_pane.getPrefWidth() / gridSize];
+                gridHandler = new GridHandler(grid_pane.getPrefWidth(), grid_pane.getPrefHeight(), gridSize, grid_pane, values, visited, statistics);
+                gridHandler.initializeGrid();
+            }
         }
 
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
             grid_pane.setStyle("-fx-background-color: lightgrey;");
             bg_pane.setStyle("-fx-background-color: lightgrey;");
+
+            gridSize = (int) cell_size_slider.getValue();
             statistics = new Statistics(text_pane, island_counter_label, cells_visited_label, island_area_Label, water_area_label, max_island_perimeter_label);
             values = new int[(int) grid_pane.getPrefHeight() / gridSize][(int) grid_pane.getPrefWidth() / gridSize];
             visited = new boolean[(int) grid_pane.getPrefHeight() / gridSize][(int) grid_pane.getPrefWidth() / gridSize];
